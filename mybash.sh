0c2bd27fb802781112f07dc34c4254db518b5653
Shamshad Abdullah <abdullas@clarkson.edu>
2022-04-04 22:00:12 -0400

    Update mybash.sh

--- mybash.sh
+++ mybash.sh
@@ -8,67 +8,66 @@ cd ..
 touch index.html
 touch feat/styles/styles.css
 touch feat/scripts/main.js
-echo " <div class="courses-container">" >> index.html
-echo "	<div class="course">" >> index.html
-echo "		<div class="course-preview">" >> index.html
-echo "			<h6>Course</h6>" >> index.html
-echo "			<h2>JavaScript Fundamentals</h2>" >> index.html
-echo "			<a href="" >> index.html
-echo "		</div>" >> index.html
-echo "		<div class="course-info">" >> index.html
-echo "			<div class="progress-container">" >> index.html
-echo "				<div class="progress"></div>" >> index.html
-echo "				<span class="progress-text">" >> index.html
-echo "					6/9 Challenges" >> index.html
-echo "				</span>" >> index.html
-echo "			</div>" >> index.html
-echo "			<h6>Chapter 4</h6>" >> index.html
-echo "			<h2>Callbacks & Closures</h2>" >> index.html
-echo "			<button class="btn">Continue</button>" >> index.html
-echo "		</div>" >> index.html
-echo "	</div>" >> index.html
-echo "</div>" >> index.html
-
-echo "<!-- SOCIAL PANEL HTML -->" >> index.html
-echo "<div class="social-panel-container">" >> index.html
-echo "	<div class="social-panel">" >> index.html
-echo "		<p>Created with <i class="fa fa-heart"></i> by" >> index.html
-echo "			<a target="_blank" href="https://florin-pop.com">Florin Pop</a></p>" >> index.html
-echo "		<button class="close-btn"><i class="fas fa-times"></i></button>" >> index.html
-echo "		<h4>Get in touch on</h4>" >> index.html
-echo "		<ul>" >> index.html
-echo "			<li>" >> index.html
-echo "				<a href="https://www.patreon.com/florinpop17" target="_blank">" >> index.html
-echo "					<i class="fab fa-discord"></i>" >> index.html
-echo "				</a>" >> index.html
-echo "			</li>" >> index.html
-echo "			<li>" >> index.html
-echo "				<a href="https://twitter.com/florinpop1705" target="_blank">" >> index.html
-echo "					<i class="fab fa-twitter"></i>" >> index.html
-echo "				</a>" >> index.html
-echo "			</li>" >> index.html
-echo "			<li>" >> index.html
-echo "				<a href="https://linkedin.com/in/florinpop17" target="_blank">" >> index.html
-echo "					<i class="fab fa-linkedin"></i>" >> index.html
-echo "				</a>" >> index.html
-echo "			</li>" >> index.html
-echo "			<li>" >> index.html
-echo "				<a href="https://facebook.com/florinpop17" target="_blank">" >> index.html
-echo "					<i class="fab fa-facebook"></i>" >> index.html
-echo "				</a>" >> index.html
-echo "			</li>" >> index.html
-echo "			<li>" >> index.html
-echo "				<a href="https://instagram.com/florinpop17" target="_blank">" >> index.html
-echo "					<i class="fab fa-instagram"></i>" >> index.html
-echo "				</a>" >> index.html
-echo "			</li>" >> index.html
-echo "		</ul>" >> index.html
-echo "	</div>" >> index.html
-echo "</div>" >> index.html
-echo "<button class="floating-btn">" >> index.html
-echo "	Get in Touch" >> index.html
-echo "</button>" >> index.html
-
-echo "<div class="floating-text">" >> index.html
-echo "	Part of <a href="https://florin-pop.com/blog/2019/09/100-days-100-projects" target="_blank">" >> index.html
-echo "</div>" >> index.html
+echo "<!DOCTYPE html>
+echo "<html>
+echo "<head>
+echo "    <title>
+echo "        Search Bar
+echo "    </title>
+echo "    <link rel="stylesheet" type="text/css" href="feat/styles/styles.css">
+echo "</head>
+echo "<body>
+echo "    <input id="searchbar" onkeyup="search_animal()" type="text"
+echo "        name="search" placeholder="Search animals..">
+echo "    <ol id='list'>
+echo "        <li class="animals">Monkey</li>
+echo "        <li class="animals">Turtle</li>
+echo "        <li class="animals">Whale</li>
+echo "        <li class="animals">Aligator</li>
+echo "        <li class="animals">Donkey</li>
+echo "        <li class="animals">Horse</li>
+echo "    </ol>
+echo "    <script src="./animals.js"></script>
+echo "</body>
+echo "</html>
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
+echo "
